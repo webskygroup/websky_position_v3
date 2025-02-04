3eb655e3414d3dd18f61ff1f684881a03a667a77
webskygroup <117711962+webskygroup@users.noreply.github.com>
2025-02-04 14:41:16 +0330

    firdt version 3

---
+++ upload/admin/language/en-gb/extension/module/websky_position.php
@@ -0,0 +1,22 @@
+<?php
+// Heading
+$_['heading_title']     = '<img src="https://opencart-ir.com/image/logo25.png" alt="webskygroup" title="webskygroup" /> change position with drag and drop';
+
+// Text
+$_['text_extension']    = 'Extensions';
+$_['text_success']      = 'Success: You have modified websky_position module!';
+$_['text_edit']         = 'Edit websky_position Module';
+
+$_['tab_setting']         = 'general setting';
+$_['tab_support']         = 'support';
+
+
+// Entry
+$_['entry_name']        = 'Module Name';
+$_['entry_title']       = 'Heading Title';
+$_['entry_description'] = 'Description';
+$_['entry_status']      = 'Status';
+
+// Error
+$_['error_permission']  = 'Warning: You do not have permission to modify websky_position module!';
+$_['error_name']        = 'Module Name must be between 3 and 64 characters!';
